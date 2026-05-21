e2a60fbc18b5af9ff21c863237e1d550668542f9
vantuh <i.mykytiuk@protonmail.com>
2026-05-22 00:56:39 +0300

    fix models loss on resume

--- pi/.pi/agent/extensions/kiro-acp/index.ts
+++ pi/.pi/agent/extensions/kiro-acp/index.ts
@@ -5,15 +5,7 @@ import { log } from "./logging.ts";
 import { stopAllSessions } from "./session-manager.ts";
 import { streamKiroAcp } from "./stream.ts";
 
-const REGISTERED_SYMBOL = Symbol.for("kiro-acp:registered");
-
 export default function (pi: ExtensionAPI) {
-	if ((globalThis as any)[REGISTERED_SYMBOL]) {
-		log("extension skipped (subagent context)", { pid: process.pid });
-		return;
-	}
-	(globalThis as any)[REGISTERED_SYMBOL] = true;
-
 	log("extension loaded", { pid: process.pid, models: KIRO_MODELS.length });
 	pi.registerProvider("kiro-acp", {
 		name: "Kiro ACP",
