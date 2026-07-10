8a400fddab2815a33cea16af6718991295b700e7
vantuh <i.mykytiuk@protonmail.com>
2026-07-11 00:42:06 +0300

    fix: pi-cursor-sdk env variables for correct tool call

--- zsh/.zshrc.d/10-env.zsh
+++ zsh/.zshrc.d/10-env.zsh
@@ -2,3 +2,13 @@
 export POWERLINE_NERD_FONTS=1
 export PLANNOTATOR_SHARE=disabled
 export PILENS_DATA_DIR=~/.pi-lens/projects
+
+# Pi cursor sdk pi extension config
+export PI_CURSOR_EXPOSE_BUILTIN_TOOLS=1
+export PI_CURSOR_PI_TOOL_BRIDGE=1
+export PI_CURSOR_SETTING_SOURCES=project,plugins,team
+export PI_CURSOR_MCP_CONNECT_TIMEOUT_SECONDS=5
+export PI_CURSOR_MCP_TOOL_TIMEOUT_SECONDS=120
+# export PI_CURSOR_PI_TOOL_BRIDGE_DEBUG=1
+# export PI_CURSOR_PI_TOOL_BRIDGE_DEBUG_FILE=/tmp/pi-cursor-bridge.ndjson
+# export PI_CURSOR_SDK_EVENT_DEBUG=1
