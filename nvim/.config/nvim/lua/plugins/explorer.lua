66a15c0442817a40e404d512a12bbd01d7fdbc63
vantuh <i.mykytiuk@protonmail.com>
2026-07-20 12:35:21 +0300

    fix(nvim): preview

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -7,6 +7,13 @@ return {
           explorer = {
             hidden = true, -- show dotfiles (e.g. .config)
             ignored = false, -- keep gitignored hidden (.DS_Store, .env, …)
+            layout = { -- show preview
+              preset = "sidebar",
+              preview = {
+                enabled = true,
+                main = true,
+              },
+            },
           },
           -- Space Space / <leader>ff
           files = {
