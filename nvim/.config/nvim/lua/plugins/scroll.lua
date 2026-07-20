0e2c87547d93841b00598bf7c75a97c9912f586c
vantuh <i.mykytiuk@protonmail.com>
2026-07-20 12:47:03 +0300

    fix(nvim): faster scroll animation

---
+++ nvim/.config/nvim/lua/plugins/scroll.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = {
+        animate = {
+          duration = { step = 5, total = 80 },
+          easing = "linear",
+        },
+        animate_repeat = {
+          delay = 50,
+          duration = { step = 2, total = 20 },
+          easing = "linear",
+        },
+      },
+    },
+  },
+}
