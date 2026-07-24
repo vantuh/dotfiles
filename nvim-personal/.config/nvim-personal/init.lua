bebf580e727daa6305b9ac511f1136ca92ea5931
vantuh <i.mykytiuk@protonmail.com>
2026-07-25 00:45:43 +0300

    fix: add git keymap set

--- nvim-personal/.config/nvim-personal/init.lua
+++ nvim-personal/.config/nvim-personal/init.lua
@@ -286,6 +286,7 @@ do
     spec = {
       { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
       { '<leader>t', group = '[T]oggle' },
+      { '<leader>g', group = '[G]it' },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
       { 'gr', group = 'LSP Actions', mode = { 'n' } },
     },
