b99006a8c8d79cae9148b3f1475f03afc0a39356
vantuh <i.mykytiuk@protonmail.com>
2026-07-26 01:32:55 +0300

    fix: delete unused plugins init lua

--- nvim-personal/.config/nvim-personal/lua/custom/plugins/init.lua
+++
@@ -1,7 +0,0 @@
-local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
-for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
-  if (type == 'file' or type == 'link') and file_name:match '%.lua$' and file_name ~= 'init.lua' then
-    local module = file_name:gsub('%.lua$', '')
-    require('custom.plugins.' .. module)
-  end
-end
