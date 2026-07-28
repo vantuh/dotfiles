3f59abf02782d494dad80d7a4a697c718fca3f30
vantuh <i.mykytiuk@protonmail.com>
2026-07-28 10:47:53 +0300

    feat: switch theme from dracula to catppuccin mocha

--- nvim-personal/.config/nvim-personal/lua/ui/colorscheme.lua
+++ nvim-personal/.config/nvim-personal/lua/ui/colorscheme.lua
@@ -1,2 +1,18 @@
-vim.pack.add { 'https://github.com/Mofiqul/dracula.nvim' }
-vim.cmd.colorscheme 'dracula'
+vim.pack.add { 'https://github.com/catppuccin/nvim' }
+
+require('catppuccin').setup {
+  flavour = 'mocha',
+  integrations = {
+    blink_cmp = true,
+    bufferline = true,
+    gitsigns = true,
+    noice = true,
+    snacks = true,
+    treesitter = true,
+    which_key = true,
+    mini = { enabled = true },
+    lsp_trouble = true,
+  },
+}
+
+vim.cmd.colorscheme 'catppuccin'
