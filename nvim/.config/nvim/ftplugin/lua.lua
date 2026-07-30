7c854a822a79cce7a798dd768830a1b07db39516
vantuh <i.mykytiuk@protonmail.com>
2026-07-30 14:22:03 +0300

    fix(nvim): cut unnecessary ftplugin

--- nvim/.config/nvim/ftplugin/lua.lua
+++
@@ -1,5 +0,0 @@
--- Buffer-local maps for Lua files (auto-sourced by Neovim on FileType).
-
-vim.keymap.set({ 'n', 'x' }, '<localleader>r', function()
-  Snacks.debug.run()
-end, { buffer = true, desc = 'Run Lua' })
